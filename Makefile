8425684062c580b750b7a3d18f9efca24d802a7f
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2025-01-25 00:58:48 +0100

    Do-nothing tp tick passthrough
    Verified that translocators still work with this.
    
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 make:
-	ls $(VS)/*.dll $(VS)/Lib/*.dll $(DOTNET)/*.dll | sed 's@^@/reference:@' | xargs csc main.cs /out:transrod.dll -t:library
+	ls $(VS)/*.dll $(VS)/Lib/*.dll $(VS)/Mods/VSSurvivalMod.dll $(VS)/Mods/VSEssentials.dll $(DOTNET)/*.dll | sed 's@^@/reference:@' | xargs csc main.cs /out:transrod.dll -t:library
 	zip -r transrod.zip assets transrod.dll modinfo.json transrod.deps.json
 
 clean:
--- main.cs
+++ main.cs
@@ -1,13 +1,60 @@
+using HarmonyLib;
+using System;
+using System.Reflection;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
 using Vintagestory.API.Config;
+using Vintagestory.API.Datastructures;
+using Vintagestory.API.MathTools;
 using Vintagestory.API.Server;
+using Vintagestory.API.Util;
+using Vintagestory.GameContent;
 
 namespace TransRod {
+	[HarmonyPatch(typeof(BlockEntityStaticTranslocator), nameof(BlockEntityStaticTranslocator.setupGameTickers))]
+	public class BlockEntityStaticTranslocatorHook {
+		static bool Prefix(BlockEntityStaticTranslocator __instance) {
+			if (__instance.Api.Side == EnumAppSide.Server)
+			{
+				var prop = __instance.GetType().GetField("sapi",
+									 System.Reflection.BindingFlags.NonPublic
+									 | System.Reflection.BindingFlags.Instance);
+				prop.SetValue(__instance, __instance.Api as ICoreServerAPI);
+
+				Action<float> doServer = delegate(float dt) {
+					var prop = __instance.GetType().GetMethod("OnServerGameTick",
+										 System.Reflection.BindingFlags.NonPublic
+										 | System.Reflection.BindingFlags.Instance);
+					prop.Invoke(__instance, new object[] { dt });
+				};
+				__instance.RegisterGameTickListener(doServer, 250);
+			}
+			else
+			{
+				Action<float> doClient = delegate(float dt) {
+					var prop = __instance.GetType().GetMethod("OnClientGameTick",
+										 System.Reflection.BindingFlags.NonPublic
+										 | System.Reflection.BindingFlags.Instance);
+					prop.Invoke(__instance, new object[] { dt });
+				};
+				__instance.RegisterGameTickListener(doClient, 50);
+			}
+			return false;
+		}
+	}
+
 	public class TransRodModSystem : ModSystem {
+		private Harmony modhook;
+
+		public TransRodModSystem() {
+			modhook = new Harmony("org.4chan.mazornoob.transrod");
+		}
+
 		public override void Start(ICoreAPI api)
 		{
 			api.Logger.Notification("Hello mod");
+			modhook.PatchAll();
 		}
 
 		public override void StartServerSide(ICoreServerAPI api)
